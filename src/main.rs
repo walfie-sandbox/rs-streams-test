8bd53012875163857d643d4009fe1ca533e07d28
Walfie <walfington@gmail.com>
2017-03-26 18:32:20 -0400

    works. uses hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 extern crate futures;
 extern crate tokio_core;
-extern crate multiqueue;
 
 use futures::{Future, Stream};
 use futures::sink::Sink;
@@ -8,37 +7,72 @@ use futures::sync::mpsc;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
+use std::fmt::Debug;
+use std::hash::Hash;
 use std::rc::Rc;
 use std::sync::Arc;
 use tokio_core::reactor::Core;
 
-#[derive(PartialEq, Hash)]
-struct Consumer;
+#[derive(Clone, Debug)]
+struct Subscriber<Id, Msg> {
+    pub id: Id,
+    tx: mpsc::UnboundedSender<Msg>,
+}
 
-impl Sink for Consumer {
-    type SinkItem = String;
-    type SinkError = ();
+#[derive(Clone, Debug)]
+struct Subscribers<Id, Msg>(Rc<RefCell<HashMap<Id, Subscriber<Id, Msg>>>>)
+    where Id: Hash + Eq + Debug;
 
-    fn start_send(
-        &mut self,
-        item: Self::SinkItem
-    ) -> futures::StartSend<Self::SinkItem, Self::SinkError> {
-        println!("{}", item);
-        Ok(futures::AsyncSink::Ready)
+impl<Id, Msg> Subscribers<Id, Msg>
+    where Id: Clone + Debug + Eq + Hash,
+          Msg: Clone + Debug + 'static
+{
+    fn new() -> Self {
+        Subscribers(Rc::new(RefCell::new(HashMap::new())))
     }
-    fn poll_complete(&mut self) -> futures::Poll<(), Self::SinkError> {
-        Ok(futures::Async::Ready(()))
+
+    fn insert(&self, subscriber: Subscriber<Id, Msg>) {
+        self.0.borrow_mut().insert(subscriber.id.clone(), subscriber);
+    }
+
+    fn remove(&self, id: &Id) -> Option<Subscriber<Id, Msg>> {
+        self.0.borrow_mut().remove(id)
+    }
+
+    fn broadcast<E: 'static>(&self, msg: Msg) -> Box<Future<Item = (), Error = E>> {
+        let subscribers = self.0.borrow();
+
+        let sends = subscribers.values().map(|subscriber| subscriber.tx.clone().send(msg.clone()));
+
+        let sends_stream = ::futures::stream::futures_unordered(sends).then(|_| Ok(()));
+
+        Box::new(sends_stream.for_each(|_| Ok(())))
     }
 }
 
 fn main() {
-    let (tx, rx) = multiqueue::broadcast_fut_queue(25);
+    let mut core = Core::new().expect("failed to instantiate Core");
+    let handle = core.handle();
 
-    let consumer = Consumer;
+    let subscribers = Subscribers::new();
 
-    let f = rx.add_stream().forward(consumer);
+    for id in 0..10 {
+        let (tx, rx) = mpsc::unbounded();
 
-    tx.try_send("Hello!".to_string()).unwrap();
-    let mut core = Core::new().unwrap();
-    core.run(f).unwrap();
+        let subscriber = Subscriber { id: id, tx: tx };
+
+        let job = rx.for_each(move |msg| {
+            println!("{}: {}", id, msg);
+            Ok(())
+        });
+
+        subscribers.insert(subscriber);
+        handle.spawn(job);
+    }
+
+    let broadcast = subscribers.broadcast::<()>("Hello world!".to_string());
+
+    let empty = futures::empty::<(), ()>();
+
+    core.run(broadcast.then(|_| empty)).expect("failed to run");
 }
